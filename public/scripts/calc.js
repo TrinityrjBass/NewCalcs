f0b01a758e32669718dc67605311db2267645381
DESKTOP-IOFF845\Trinity <trinityrjbass@gmail.com>
2020-10-13 01:11:44 -0500

    Database updates, some UI improvements.

--- public/scripts/calc.js
+++ public/scripts/calc.js
@@ -1,48 +1,50 @@
-// Database Reference Object
-var dbReference = firebase.database().ref("equations");
-
-// sync changes
-dbReference.on('value', snap =>
-    document.getElementById('serverOut').value = JSON.stringify(snap.val(), null, 3));
-
-// dbReference.limitToLast(10).on("child_added", function(snapshot) {
-//     // This callback will be triggered exactly two times, unless there are
-//     // fewer than two dinosaurs stored in the Database. It will also get fired
-//     // for every new, heavier dinosaur that gets added to the data set.
-//     console.log(snapshot.key);
-// });
 // make a calculator
 
+// create a db references
+var dbReference = firebase.database().ref("equations");
+var dbFilter = dbReference.limitToLast(10);
+
+// bool flag for clearing display
+var finished = false;
 // to store equation value
-var equation = { text: "", id: 0 };
+var equation = "";
 
 // equation list from server
 var equations = [];
-var dbequations;
 
-// get list of equations
+// magic update when new entry is made in db
+dbFilter.on("child_added", snap => {
+    // populates array with equations from database
+    if (snap.val().newEquation !== undefined) {
+        equations.push(snap.val().newEquation);
+    }
+
+    // get the data from the response
+    if (equations.length == 0) {
+        console.log("1 + 1 = 11, there are no equations from db");
+    }
+});
+
+// get list of equations from db
 function getEquations() {
-    console.log("getting data");
-    let dbItems = dbReference.limitToLast(10);
-    var eq = [];
-    dbItems.on("value", snap => {
-        // this should populate the eq array
-        var e = snap.val();
-        e.foreach(function(item, index) {
-            eq[index] = item;
-        })
+    var dbFilter = dbReference.limitToLast(10);
+    console.log("retrieving data");
 
-        if (eq.id == 0) {
-            console.log("1 + 1 = 11, there are no equations from db");
-        } else {
-            console.log(eq.text + " key: " + eq.key)
+    dbFilter.on("value", snap => {
+            // populates array with equations from database
+            if (snap.val().newEquation !== undefined) {
+                equations.push(snap.val().newEquation);
+            }
+            // get the data from the response
+            if (equations.length == 0) {
+                console.log("1 + 1 = 11, there are no equations from db");
+            }
+            // show us what you found :)
+            displayResults(equations);
         }
-    })
 
-    return
-    // dbReference.on('value', snap =>
-    //     t = JSON.stringify(snap.val(), null, 3));
-    console.log(t);
+    )
+
 }
 
 // send new equation to DB
@@ -53,72 +55,64 @@ function sendEquation(newEquation) {
     equationref.set({ newEquation });
 }
 
+function build(n) {
+    if (finished == false) {
+        // keep building equation
+        equation += n;
+    } else {
+        // new equation, clear old one
+        clearEquation();
+        equation = n;
+        finished = false;
+    }
+    document.getElementById("calcDisplay").value = equation;
+}
+
 // Calulate the equation
 function calculate() {
-    // get operands
-    let o1 = document.getElementById("operand1").value;
-    let o2 = document.getElementById("operand2").value;
-
-    let operator = document.getElementById("operator").value;
-
-    // parse operands to integers to avoid stringy problems
-    o1 = parseInt(o1);
-    o2 = parseInt(o2);
-
-    let equation = o1 + operator + o2;
-
+    finished = true;
     // sum, difference, product, quotient
-    let result = 0;
+    let result = '';
 
     // calculate equation
     try {
         result = "=" + eval(equation);
-        equation += result + '';
+        equation += result;
+
+        // show full equation on calculator
+        document.getElementById("calcDisplay").value = equation;
+
         // add equation to db
         sendEquation(equation);
     } catch (e) {
         result = "error calculating " + equation;
+        // show everyone the error
+        sendEquation(result);
     }
 
     // add to display box
     displayResults(equations);
 }
-
-// deprecated
-function checkRows(equations) {
-    var len;
-    if (equations.length !== null) {
-        len = equations.length;
-    } else {
-        len = 0;
-    }
-
-    console.log(len);
-
-    if (len > 9) {
-        equations.shift();
-    }
-
-}
-
+// 
 function displayResults(equations) {
-    // clear the text area
-    clearHistory();
-    // get data and refill the text area
-    getEquations();
-    // let l = equations.length;
-    // for (l; l > 0; l--) {
-    //     // print in reverse to textarea
-    //     document.getElementById("display").value += equations[l - 1];
-    //}
+    // clear the text area so we don't duplicate entries
+    document.getElementById("display").value = ''
+
+    // and repopulate the text area
+    if (equations.length === undefined) {
+        getEquations();
+    }
+    let l = equations.length;
+    for (l; l > 0; l--) {
+        // print in reverse order to textarea
+        document.getElementById("display").value += equations[l - 1] + '\r\n';
+    }
 
 }
 // provide a clean text area to redisplay/update equations
-function clearHistory() {
-    document.getElementById("display").value = "";
+function clearEquation() {
+    equation = '';
+    document.getElementById("calcDisplay").value = '';
 }
 
-function print(item, index) {
-    console.log(index + " : " + item);
-
-}
+$(document).ready(getEquations());
